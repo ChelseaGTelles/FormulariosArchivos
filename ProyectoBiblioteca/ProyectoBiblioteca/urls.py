13ae76ee64256659d702422de357d4a16f87b7b4
chels <s22120160@alumnos.itsur.edu.mx>
2025-09-25 19:36:49 -0600

    Rutas estáticas.

--- ProyectoBiblioteca/ProyectoBiblioteca/urls.py
+++ ProyectoBiblioteca/ProyectoBiblioteca/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('libros.urls')),  # así la app estará en la raíz
+    path('', include('libros.urls')),  
 ]
 
 if settings.DEBUG:
